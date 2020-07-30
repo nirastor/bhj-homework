2f9b559feb26e2f85af3ed834b33ca882bd9a672
Nikita Rastorguev <nirastor@gmail.com>
2020-07-30 22:40:50 +0300

    task menu revisions

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,11 @@
-const elMainMenuItems = Array.from(document.querySelectorAll('.menu_main .menu__item'));
+const elMainMenuItems = Array.from(document.querySelectorAll('.menu_main > .menu__item > .menu__link'));
+console.log(elMainMenuItems.length);
 const elMainMenuAllSubmenu = Array.from(document.querySelectorAll('.menu_main .menu_sub'));
 
 
 for(let item of elMainMenuItems) {
     item.onclick = function () {
-        const itemSubmenu = item.querySelector('.menu_sub');
+        const itemSubmenu = item.parentNode.querySelector('.menu_sub');
         
         if (!itemSubmenu) {
             return;
