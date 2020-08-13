f97d54ea69c555ebdb0369dfbc444f8a1546206d
Nikita Rastorguev <nirastor@gmail.com>
2020-08-13 14:31:28 +0300

    task cart done (lvl-1 and lvl-3)

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,10 +2,39 @@ const products = Array.from(document.querySelectorAll('.product'));
 const elCart = document.querySelector('.cart');
 const elCartProducts = document.querySelector('.cart__products');
 const templateCart = document.getElementById('teplate-product-in-chart').content.querySelector('.cart__product');
-let inChart = [];
+let inCart = [];
+
+function loadCartFromLocalStorage() {
+    for (let i = 1; i <= 4; i++) {
+        if (localStorage.getItem(i)) {
+            showNewProductInCart(i, localStorage.getItem(i), products[i-1]);
+        }
+    }
+}
+
+function showNewProductInCart(id, quantity, product) {
+    const newProdInCart = templateCart.cloneNode(1);
+    const elChartProductCount = newProdInCart.querySelector('.cart__product-count');
+    newProdInCart.dataset.id = id;
+    newProdInCart.querySelector('.cart__product-image').src = product.querySelector('.product__image').src;
+    elChartProductCount.textContent = quantity;
+    elCartProducts.appendChild(newProdInCart);
+    inCart.push({id: id, quantity: quantity});
+    
+    const elDeleteProduct = newProdInCart.querySelector('.cart__remove');
+    elDeleteProduct.addEventListener('click', (e) => {
+        e.preventDefault();
+        newProdInCart.remove();
+        const delId = elDeleteProduct.closest('.cart__product').dataset.id;
+        const indexInCartForDel = inCart.findIndex(item => item.id === id);
+        inCart.splice(indexInCartForDel, 1);
+        localStorage.removeItem(id);
+        showCart();
+    })
+}
 
 function showCart() {
-    if (inChart.length) {
+    if (inCart.length) {
         elCart.classList.remove('cart--hide');
     } else {
         elCart.classList.add('cart--hide');
@@ -35,35 +64,22 @@ products.forEach(product => {
         e.preventDefault();
         const id = product.dataset.id;
         const quantity = Number(elProdValue.textContent.trim());
-        const ProdIsInChart = inChart.find(item => item.id === id);
+        const ProdIsInCart = inCart.find(item => item.id === id);
 
-        if (ProdIsInChart) {
-            ProdIsInChart.quantity = Number(ProdIsInChart.quantity) + quantity;
-            const productsInChart = Array.from(elCartProducts.querySelectorAll('.cart__product'));
-            const product = productsInChart.find(item => item.dataset.id === id);
-            product.querySelector('.cart__product-count').textContent = ProdIsInChart.quantity;
+        if (ProdIsInCart) {
+            ProdIsInCart.quantity = Number(ProdIsInCart.quantity) + quantity;
+            localStorage.setItem(id, Number(ProdIsInCart.quantity));
+            const productsInCart = Array.from(elCartProducts.querySelectorAll('.cart__product'));
+            const product = productsInCart.find(item => item.dataset.id === id);
+            product.querySelector('.cart__product-count').textContent = ProdIsInCart.quantity;
         } else {
-            const newProdInChart = templateCart.cloneNode(1);
-            const elChartProductCount = newProdInChart.querySelector('.cart__product-count');
-            newProdInChart.dataset.id = id;
-            newProdInChart.querySelector('.cart__product-image').src = product.querySelector('.product__image').src;
-            elChartProductCount.textContent = quantity;
-            elCartProducts.appendChild(newProdInChart);
-            inChart.push({id: id, quantity: quantity});
-
-            const elDeleteProduct = newProdInChart.querySelector('.cart__remove');
-            elDeleteProduct.addEventListener('click', (e) => {
-                e.preventDefault();
-                newProdInChart.remove();
-                const delId = elDeleteProduct.closest('.cart__product').dataset.id;
-                const indexInCartForDel = inChart.findIndex(item => item.id === id);
-                inChart.splice(indexInCartForDel, 1);
-                showCart();
-            })
+            showNewProductInCart(id, quantity, product);
+            localStorage.setItem(id, quantity);
         }
 
         showCart();
     });
 });
 
+loadCartFromLocalStorage();
 showCart();
