1fe3edb2ce7dc62b5add48bf16e3241ce5059cbe
Nikita Rastorguev <nirastor@gmail.com>
2020-08-17 14:05:44 +0300

    task progressbar done (with questions)

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -5,16 +5,25 @@ console.log(elProgress.value);
 
 elForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    elProgress.value = 0.2;
-    console.log(elProgress.value, 'submit');
+    // elProgress.value = 0.2;
+    // console.log(elProgress.value, 'submit');
     const formData = new FormData(elForm);
     const xhrSendForm = new XMLHttpRequest();
     xhrSendForm.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    xhrSendForm.addEventListener('readystatechange', () => {
-        elProgress.value += 0.2;
-        console.log('progress', elProgress.value);
-        console.log('readyState', xhrSendForm.readyState);
+    
+    xhrSendForm.upload.addEventListener('progress', (e) => {
+        elProgress.value += e.loaded / e.total;
+        console.log(elProgress.value);
     });
+
+    xhrSendForm.addEventListener('readystatechange', () => {
+        console.log('readyState', xhrSendForm.readyState);
+        if (xhrSendForm.readyState === XMLHttpRequest.DONE && xhrSendForm.status === 200) {
+           console.log('done');
+        }
+    });
+
+    
     xhrSendForm.send(formData);
     elForm.reset();
 });
