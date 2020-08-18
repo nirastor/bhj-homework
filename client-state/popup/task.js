0a3d5a2ca5dadd42dd47cfc27c00b9cb1e3c1bf0
Nikita Rastorguev <nirastor@gmail.com>
2020-08-18 12:07:28 +0300

    task popup done

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,17 +3,11 @@ const elSubscribeModalClose = elSubscribeModal.querySelector('.modal__close');
 
 elSubscribeModalClose.addEventListener('click', () => {
     elSubscribeModal.classList.remove('modal_active');
-    // *** Не записывается кука в хроме ***
-    document.cookie = 'modalsubscribewasclosed=any';
-    console.log(document.cookie);
+    document.cookie = 'mynewcoocie=any; Expires=Mon, 31 Aug 2020 21:41:37 GMT';
 });
 
 function wasShowModal() {
-    console.log(document.cookie);
-    console.log((";" + document.cookie));
-    console.log((";" + document.cookie).includes(';modalSubscribeWasClosed='));
-
-    return (";" + document.cookie).includes(';modalSubscribeWasClosed=');
+    return (";" + document.cookie).includes('; mynewcoocie=');
 }
 
 function showSubscribeModal() {
@@ -23,11 +17,3 @@ function showSubscribeModal() {
 }
 
 showSubscribeModal();
-
-// *** Не записывается кука в хроме ***
-console.log('1');
-document.cookie = 'setnewcookie=data';
-document.cookie = 'setnewcookie2=data2';
-console.log('2');
-console.log('kuka', document.cookie);
-console.log('3');
